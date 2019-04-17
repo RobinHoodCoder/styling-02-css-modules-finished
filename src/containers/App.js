4df1007948c9eea0c98a5aee73d9eea506d27ac1
RobinHoodCoder <14Januari>
2019-04-17 20:52:15 +0200

    Changed a whole lot!

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from '../Persons/Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   state = {
@@ -48,38 +49,32 @@ class App extends Component {
     let persons = null;
     let btnClass = '';
 
+    let cockpit = null;
+
+    cockpit = (
+     <Cockpit
+      persons={this.state.persons}
+      clicked={this.togglePersonsHandler}
+     />
+    )
+
     if ( this.state.showPersons ) {
       persons = (
-        <div>
-          {this.state.persons.map( ( person, index ) => {
-            return <Person
-              click={() => this.deletePersonHandler( index )}
-              name={person.name}
-              age={person.age}
-              key={person.id}
-              changed={( event ) => this.nameChangedHandler( event, person.id )} />
-          } )}
-        </div>
+          <Persons
+              persons={this.state.persons}
+              clicked={this.deletePersonHandler}
+              changed={this.nameChangedHandler}
+          />
       );
 
       btnClass = classes.Red;
     }
 
-    const assignedClasses = [];
-    if ( this.state.persons.length <= 2 ) {
-      assignedClasses.push( classes.red ); // classes = ['red']
-    }
-    if ( this.state.persons.length <= 1 ) {
-      assignedClasses.push( classes.bold ); // classes = ['red', 'bold']
-    }
+
 
     return (
         <div className={classes.App}>
-          <h1>Hi, I'm a React App</h1>
-          <p className={assignedClasses.join( ' ' )}>This is really working!</p>
-          <button
-            className={btnClass}
-            onClick={this.togglePersonsHandler}>Toggle Persons</button>
+          {cockpit}
           {persons}
         </div>
     );
